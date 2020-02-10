aa65a2bfbc7624dbc5ed8956b2bb6434a4669ec4
jussi.kurki <jussi.kurki@gofore.com>
2020-02-10 14:19:02 +0200

    Refactor editor toolbar to own file

--- lakieditori-ui/src/components/RichTextEditor.tsx
+++ lakieditori-ui/src/components/RichTextEditor.tsx
@@ -1,25 +1,16 @@
-import React, {CSSProperties, useEffect, useMemo, useRef, useState} from "react";
-import {createEditor, Editor, Node as SlateNode, Range} from 'slate'
+import React, {CSSProperties, useEffect, useMemo, useState} from "react";
+import {createEditor, Node as SlateNode} from 'slate'
 import {
   Editable,
   ReactEditor,
   RenderElementProps,
   RenderLeafProps,
   Slate,
-  useSlate,
   withReact
 } from 'slate-react'
 import {withHistory} from "slate-history";
-import {css} from "emotion";
-import {Button, Icon, Menu, Portal} from "./RichTextEditorUtilComponents";
-import {
-  deserialize,
-  insertLink,
-  isFormatActive,
-  isLinkActive,
-  serialize,
-  toggleFormat
-} from "./RichTextEditorFunctions";
+import {deserialize, serialize, toggleFormat} from "./RichTextEditorFunctions";
+import HoveringToolbar from "./RichTextEditorHoveringToolbar";
 
 const RichTextEditor: React.FC<Props> = ({value, onChange = () => null, placeholder, style}) => {
   const [initialized, setInitialized] = useState<boolean>(false);
@@ -110,108 +101,5 @@ const EditorLeaf = ({attributes, children, leaf}: RenderLeafProps) => {
   }
   return <span {...attributes}>{children}</span>
 };
-
-const HoveringToolbar = () => {
-  const ref = useRef<HTMLDivElement>();
-  const editor = useSlate();
-
-  useEffect(() => {
-    const el = ref.current;
-    const {selection} = editor;
-
-    if (!el) {
-      return;
-    }
-
-    if (!selection ||
-        !ReactEditor.isFocused(editor) ||
-        Range.isCollapsed(selection) ||
-        Editor.string(editor, selection) === '') {
-      el.removeAttribute('style');
-      return;
-    }
-
-    const domSelection = window.getSelection();
-
-    if (!domSelection || !domSelection.rangeCount) {
-      return;
-    }
-
-    const domRange = domSelection.getRangeAt(0);
-    const rect = domRange.getBoundingClientRect();
-
-    if (rect) {
-      el.style.opacity = '1';
-      el.style.top = `${rect.top + window.pageYOffset - el.offsetHeight}px`;
-      el.style.left = `${rect.left + window.pageXOffset - el.offsetWidth / 2 + rect.width / 2}px`;
-    }
-  });
-
-  return (
-      <Portal>
-        <Menu
-            ref={ref}
-            className={css`
-          padding: 8px 7px 6px;
-          position: absolute;
-          z-index: 1;
-          top: -10000px;
-          left: -10000px;
-          margin-top: -6px;
-          opacity: 0;
-          background-color: #333;
-          border-radius: 4px;
-          transition: opacity 0.75s;
-        `}
-        >
-          <FormatButton format="bold" icon="format_bold"/>
-          <FormatButton format="italic" icon="format_italic"/>
-          <LinkButton/>
-        </Menu>
-      </Portal>
-  )
-};
-
-const FormatButton = ({format, icon}: FormatButtonProps) => {
-  const editor = useSlate();
-  return (
-      <Button
-          active={isFormatActive(editor, format)}
-          onMouseDown={event => {
-            event.preventDefault();
-            toggleFormat(editor, format);
-          }}>
-        <Icon>{icon}</Icon>
-      </Button>
-  );
-};
-
-const LinkButton = () => {
-  const editor = useSlate();
-  return (
-      <Button
-          active={isLinkActive(editor)}
-          onMouseDown={event => {
-            event.preventDefault();
-            if (!isLinkActive(editor)) {
-              insertLink(editor, window.prompt('Enter the URL of the link:'));
-            } else {
-              const [link] = Array.from(Editor.nodes(editor, {
-                match: n => n.type === 'link'
-              }));
-              const oldUrl = link[0].url;
-              const newUrl = window.prompt('Enter the URL of the link:', oldUrl);
-              insertLink(editor, newUrl === null ? oldUrl : newUrl);
-            }
-          }}>
-        <Icon>link</Icon>
-      </Button>
-  )
-};
-
-interface FormatButtonProps {
-  format: string,
-  icon: string
-}
 
 export default RichTextEditor;
