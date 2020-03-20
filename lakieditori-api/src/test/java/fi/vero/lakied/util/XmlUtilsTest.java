c7783deeaefa793ed5b27c86bae1af8844e9e484
jussi.kurki <jussi.kurki@gofore.com>
2020-03-20 13:15:56 +0200

    Add test to xml utils

--- lakieditori-api/src/test/java/fi/vero/lakied/util/XmlUtilsTest.java
+++ lakieditori-api/src/test/java/fi/vero/lakied/util/XmlUtilsTest.java
@@ -1,6 +1,7 @@
 package fi.vero.lakied.util;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import fi.vero.lakied.util.xml.XmlUtils;
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,17 @@ class XmlUtilsTest {
   void shouldParseSimpleXml() {
     Document document = XmlUtils.parseUnchecked("<hello>World!</hello>");
     assertEquals("World!", document.getElementsByTagName("hello").item(0).getTextContent());
+  }
+
+  @Test
+  void shouldQueryText() {
+    Document document = XmlUtils.parseUnchecked("<hello id=\"123\">World!</hello>");
+
+    assertEquals("World!", XmlUtils.queryText(document, "/hello"));
+    assertEquals("123", XmlUtils.queryText(document, "/hello/@id"));
+
+    assertTrue(XmlUtils.queryText(document, "/hello/bar").isEmpty());
+    assertTrue(XmlUtils.queryText(document, "/hello/@foo").isEmpty());
   }
 
 }
